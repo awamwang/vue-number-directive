48fbf379a0eceb80e1dd50659e1752859481d3c3
awam.m.wang <awam.m.wang@cn.pwc.com>
2020-09-17 11:59:22 +0800

    temp

--- examples/stories/element/stories/Button.stories.js
+++ examples/stories/element/stories/Button.stories.js
@@ -1,47 +1,35 @@
-import { action } from '@storybook/addon-actions';
-import MyButton from './MyButton.vue';
+import { action } from '@storybook/addon-actions'
+import MyButton from './MyButton.vue'
 
 export default {
   title: 'MyButton',
   component: MyButton,
-  parameters: {
+  parameters: {}
+}
 
-  }
-};
-
-import ButtonMd from './Button.md';
+import ButtonMd from './Button.md'
 export const 基础使用 = () => ({
-	components: {
-		MyButton,
-	},
-	template: `
+  components: {
+    MyButton
+  },
+  template: `
     <my-button text="点我" @btnClick="btnClick"/>
     `,
   methods: {
     btnClick: action('btnClick')
   }
-});
-
-基础使用.story = {
-  parameters: {
-    notes: { ButtonMd },
-  }
-};
-
-
-export const 使用viewport插件 = () => ({
-	components: {
-		MyButton,
-	},
-	template: `
-    <my-button text="点我"/>
-    `,
 })
-使用viewport插件.story = {
-  parameters: {
-    },
-  },
+
+基础使用.parameters = {
+  notes: { ButtonMd }
 }
 
-
-
+export const 使用viewport插件 = () => ({
+  components: {
+    MyButton
+  },
+  template: `
+    <my-button text="点我"/>
+    `
+})
+使用viewport插件.parameters = {}
